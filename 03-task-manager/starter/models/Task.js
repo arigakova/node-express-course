a8d73b82bdb6946af4c08cbf236331df436a56a6
Anna Ryzhakova <arigakova@gmail.com>
2023-11-14 13:17:57 -0500

    week6

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose')
 
-
 const TaskSchema = new mongoose.Schema({
-  name:String, comleted:Boolean
+  name: {
+    type: String,
+    required: [true, 'must provide name'],
+    trim: true,
+    maxlength: [20, 'name can not be more than 20 characters'],
+  },
+  comleted: {
+    type: Boolean,
+    default: false,
+  },
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
